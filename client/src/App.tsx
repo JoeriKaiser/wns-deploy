7d252aa09094e0f92ad2b77d77464ba431c1812d
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-03-28 11:51:54 +0200

    typo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD</h1>
+          <h1>Groupe 4 Book with CD</h1>
         </Container>
       </Header>
       <Container>
